61d157954e28f80334c4f8659085d5200770270c
Vincent Schoener <vincent.schoener@gmail.com>
2020-10-06 10:40:30 +0200

    Improve tests using parametrize

--- tests/test_atom.py
+++ tests/test_atom.py
@@ -1,15 +1,24 @@
+from pytest import mark
+
 from src.atom import reduce_atoms
 
 
-def test_reduce_atoms_simple():
-    assert reduce_atoms("HO") == {"H": 1, "O": 1}
-
-
-def test_reduce_atoms_longer():
-    assert reduce_atoms("KKKKKOOOOMgMgCiCiNNNNN") == {
-        "Ci": 2,
-        "K": 5,
-        "Mg": 2,
-        "N": 5,
-        "O": 4,
-    }
+def describe_reduce_atoms():
+    @mark.parametrize(
+        "molecule, expected",
+        [
+            ("HO", {"H": 1, "O": 1}),
+            (
+                "KKKKKOOOOMgMgCiCiNNNNN",
+                {
+                    "Ci": 2,
+                    "K": 5,
+                    "Mg": 2,
+                    "N": 5,
+                    "O": 4,
+                },
+            ),
+        ],
+    )
+    def test_fixtures(molecule, expected):
+        assert reduce_atoms(molecule) == expected
--- tests/test_molecule.py
+++ tests/test_molecule.py
@@ -1,56 +1,56 @@
-from pytest import raises
+from pytest import raises, mark
 
 from src.molecule import flatten, flatten_all, flatten_sub_molecule
 from src.exceptions.enclosure_exception import EnclosureException
 
 
-def test_flatten_should_return_same_if_nothing_changed():
-    assert flatten("MgO") == "MgO"
+def describe_flatten():
+    @mark.parametrize(
+        "molecule, expected",
+        [
+            ("MgO", "MgO"),  # Nothing to change
+            ("H2O", "HHO"),  # Simple case
+            ("Mg3H2OCi4", "MgMgMgHHOCiCiCiCi"),  # More longer
+            ("K4[ON(SO3)2]2", "KKKK[ON(SOOO)2]2"),  # Complex inside encloser
+        ],
+    )
+    def test_fixtures(molecule, expected):
+        assert flatten(molecule) == expected
 
 
-def test_flatten_should_convert_simple_molecule():
-    assert flatten("H2O") == "HHO"
+def describe_flatten_sub():
+    @mark.parametrize(
+        "molecule, expected",
+        [
+            ("Mg2OH", "Mg2OH"),  # Nothing to change
+            ("Mg(OH)2", "MgOHOH"),  # Simple case
+            ("Mg(OH)", "MgOH"),  # Simple case without a number
+            ("KKKK[ON(SOOO)2]2", "KKKKONSOOOSOOOONSOOOSOOO"),
+            (
+                "K4[ON(SO3)2]2",
+                "K4[ON(SO3)2]2",
+            ),  # To show we need to flatten before, otherwise it does nothing
+        ],
+    )
+    def test_fixtures(molecule, expected):
+        assert flatten_sub_molecule(molecule) == expected
+
+    def test_flatten_sub_molecule_bad_encloser():
+        with raises(EnclosureException) as exception:
+            flatten_sub_molecule("Mg(OH}2")
+
+        assert exception.value.message == "Parsing error"
+        assert exception.value.match.group(0) == "(OH}2"
 
 
-def test_flatten_should_convert_complex_molecule():
-    assert flatten("Mg3H2OCi4") == "MgMgMgHHOCiCiCiCi"
-
-
-def test_flatten_should_convert_inside_sub_molecule():
-    assert flatten("K4[ON(SO3)2]2") == "KKKK[ON(SOOO)2]2"
-
-
-def test_flatten_sub_molecule_should_convert_simple_flatten_molecule():
-    assert flatten_sub_molecule("Mg(OH)2") == "MgOHOH"
-
-
-def test_flatten_sub_molecule_should_convert_with_no_number():
-    assert flatten_sub_molecule("Mg(OH)") == "MgOH"
-
-
-def test_flatten_sub_molecule_should_convert_flatten_molecule():
-    assert flatten_sub_molecule("KKKK[ON(SOOO)2]2") == "KKKKONSOOOSOOOONSOOOSOOO"
-
-
-def test_flatten_sub_molecule_should_return_same_if_nothing_changed():
-    assert flatten_sub_molecule("Mg2OH") == "Mg2OH"
-
-
-def test_flatten_sub_molecule_should_throw_with_bad_encloser():
-    with raises(EnclosureException) as exception:
-        flatten_sub_molecule("Mg(OH}2")
-
-    assert exception.value.message == "Parsing error"
-    assert exception.value.match.group(0) == "(OH}2"
-
-
-def test_flatten_all_should_convert_all_molecules_with_easier():
-    assert flatten_all("Mg(OH)2") == "MgOHOH"
-
-
-def test_flatten_all_should_convert_all_molecules_with_simple():
-    assert flatten_all("Mg2OH") == "MgMgOH"
-
-
-def test_flatten_all_should_convert_all_molecules_with_complex():
-    assert flatten_all("K4[ON(SO3)2]2") == "KKKKONSOOOSOOOONSOOOSOOO"
+def describe_flatten_all():
+    @mark.parametrize(
+        "molecule, expected",
+        [
+            ("H2O", "HHO"),  # Simple case
+            ("Mg(OH)2", "MgOHOH"),  # With sub molecule
+            ("K4[ON(SO3)2]2", "KKKKONSOOOSOOOONSOOOSOOO"),  # Complex case
+        ],
+    )
+    def test_fixtures(molecule, expected):
+        assert flatten_all(molecule) == expected
